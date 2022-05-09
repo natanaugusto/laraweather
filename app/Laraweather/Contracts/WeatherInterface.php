68c876edd940808fd626d1dece78b3c4ec721a0d
Natan Augusto <natanaugusto@gmail.com>
2022-05-09 18:26:04 -0300

    Implemented more function to attributes on WeatherInterface.php (I hope to get this job)

--- app/Laraweather/Contracts/WeatherInterface.php
+++ app/Laraweather/Contracts/WeatherInterface.php
@@ -10,6 +10,12 @@ interface WeatherInterface
     function getName(): ?string;
     function setName(string $value): void;
 
+    function getCountry(): ?string;
+    function setCountry(string $value): void;
+
+    function getDescription(): ?string;
+    function setDescription(string $value): void;
+
     function getLonLat(): ?array;
     function setLonLat(float $lon, float $lat): void;
 
@@ -21,4 +27,7 @@ interface WeatherInterface
 
     function getFeels(): ?float;
     function setFeels(float $value): void;
+
+    function getRaw(): ?array;
+    function setRaw(array $value): void;
 }
--- tests/Laraweather/ClientTest.php
+++ tests/Laraweather/ClientTest.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Client\Response as HttpClientResponse;
 use Illuminate\Support\Facades\Http;
 $weather = new class implements \App\Laraweather\Contracts\WeatherInterface {
     private array $args;
+    private array $raw;
 
     public function getArgs(): ?array
     {
@@ -24,6 +25,26 @@ $weather = new class implements \App\Laraweather\Contracts\WeatherInterface {
     public function setName(string $value): void
     {
         $this->args['name'] = $value;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->args['country'] ?? null;
+    }
+
+    public function setCountry(string $value): void
+    {
+        $this->args['country'] = $value;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->args['description'] ?? null;
+    }
+
+    public function setDescription(string $value): void
+    {
+        $this->args['description'] = $value;
     }
 
     public function getLonLat(): ?array
@@ -69,6 +90,16 @@ $weather = new class implements \App\Laraweather\Contracts\WeatherInterface {
     {
         $this->args['feels'] = $value;
     }
+
+    public function getRaw(): ?array
+    {
+        return $this->raw ?? null;
+    }
+
+    public function setRaw(array $value): void
+    {
+        $this->raw = $value;
+    }
 };
 $driver = new class implements \App\Laraweather\Contracts\DriverInterface
 {
@@ -100,6 +131,18 @@ $driver = new class implements \App\Laraweather\Contracts\DriverInterface
 
     public function toWeather(HttpClientResponse $response, WeatherInterface $weather = null): WeatherInterface
     {
+        $body = json_decode(
+            json: $response->body(),
+            associative: true
+        );
+        $weather->setRaw(value: $body);
+        $weather->setName(value: $body['name']);
+        $weather->setCountry(value: $body['sys']['country']);
+        $weather->setDescription(value: $body['weather'][0]['description']);
+        $weather->setLonLat(lon: $body['coord']['lon'], lat: $body['coord']['lat']);
+        $weather->setMin(value: $body['main']['temp_min']);
+        $weather->setMax(value: $body['main']['temp_max']);
+        $weather->setFeels(value: $body['main']['feels_like']);
         return $weather;
     }
 };
@@ -107,8 +150,31 @@ $driver = new class implements \App\Laraweather\Contracts\DriverInterface
 test(description: 'Instaciate Laraweather/Client', closure: function ($weatherReturn) use ($driver, $weather) {
     mockHttp(body: $weatherReturn);
     $laraweather = new Client(driver: $driver, weather: $weather);
+    $weather = $laraweather->getByCity(name: 'Franco da Rocha');
     $this->assertInstanceOf(
         expected: WeatherInterface::class,
-        actual: $laraweather->getByCity(name: 'Franco da Rocha')
+        actual: $weather
+    );
+    $this->assertEquals(
+        expected: [
+            'name' => 'Franco da Rocha',
+            'country' => 'BR',
+            'description' => 'scattered clouds',
+            'lon' => -46.7269,
+            'lat' => -23.3217,
+            'min' => 288.48,
+            'max' => 292.63,
+            'feels' => 290.57
+        ],
+        actual: $weather->getArgs()
+    );
+
+    $meta = stream_get_meta_data(stream: $weatherReturn);
+    $this->assertEquals(
+        expected: json_decode(
+            json: file_get_contents(filename: $meta['uri']),
+            associative: true
+        ),
+        actual: $weather->getRaw()
     );
 })->with(data: 'weatherapi/openweatherapi/weather');
